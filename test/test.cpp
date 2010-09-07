113f33b8cf5a9db973196c11698b14bce34b178d
freemaul <freemaul@hotmail.fr>
2010-09-07 21:39:54 +0200

    Modification pour utilisation image JPG

--- test/test.cpp
+++ test/test.cpp
@@ -15,7 +15,7 @@ int main(int argc,char *argv[])
 	sf::Image image;
 	GefMoteur::GefRenderSfml::SpriteSfml monsp(App);
 
-	image.LoadFromFile("test.bmp");
+	image.LoadFromFile("test.jpg");
 	monsp.SetImage(image);
 
 	bool Running = true;
