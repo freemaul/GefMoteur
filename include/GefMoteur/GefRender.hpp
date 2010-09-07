889df4cb88ca31b640a89b2328e40ccf3c2a1179
freemaul <freemaul@hotmail.fr>
2010-09-07 19:48:07 +0200

    Modification lisibilité

--- include/GefMoteur/GefRender.hpp
+++ include/GefMoteur/GefRender.hpp
@@ -8,13 +8,6 @@ namespace GefRender
 	class Dessinable;
 	class RenderTarget;
 
-
-	class RenderTarget
-	{
-	public:
-		virtual void Dessine(Dessinable&)=0;
-	};
-
 	class Dessinable
 	{
 	protected:
@@ -23,6 +16,12 @@ namespace GefRender
 		Dessinable();
 		virtual void Dessine(RenderTarget&)=0;
 		void Cache(bool);
+	};
+
+	class RenderTarget
+	{
+	public:
+		virtual void Dessine(Dessinable&)=0;
 	};
 
 }; // GefRender
