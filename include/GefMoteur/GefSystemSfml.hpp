df2d7dbf56c4c5134e18e6674a1849766d1778aa
freemaul <freemaul@hotmail.fr>
2010-09-08 20:29:07 +0200

    Ajout de la classe HorlogeSfml

---
+++ include/GefMoteur/GefSystemSfml.hpp
@@ -0,0 +1,23 @@
+#ifndef GEFMOTEUR_GEFSYSTEMSFML_HPP
+#define GEFMOTEUR_GEFSYSTEMSFML_HPP
+
+#include <GefMoteur/GefSystem.hpp>
+
+namespace GefMoteur
+{
+namespace GefSystemSfml
+{
+	class HorlogeSfml : public GefSystem::Horloge
+	{
+	protected:
+		sf::Clock clock;
+	public:
+		virtual void Zero();	// remise a zero du compteur
+		virtual float Temps();	// donne le temps passé en seconde (nombre rationel)
+	};
+
+}; // GefSystemSfml
+}; // GefMoteur
+
+#endif // GEFMOTEUR_GEFSYSTEM_HPP
+
