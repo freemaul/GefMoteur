90b66c34c25bbe629667fd6080795006a06e2c8a
freemaul <freemaul@hotmail.fr>
2010-09-10 23:09:53 +0200

    Lisibilité

--- test/testsfml/test.cpp
+++ test/testsfml/test.cpp
@@ -20,16 +20,8 @@ int main(int argc,char *argv[])
 	image2.LoadFromFile("test.bmp");
 	image.LoadFromFile("mario.png");
 	monsp.Definit_Image(&image2);
-//	animation1.Definit_Image((void*)&image,6,4,(float)1.0);
 	animation1.Definit_Image_plus(&image,6,4,1.0);
 
-/*
-	animation1.nb_image_x = 6;
-	animation1.nb_image_y = 4;
-	animation1.ta_image_x = image.GetWidth();
-	animation1.ta_image_y = image.GetHeight();
-	animation1.duree = 1;
-*/
 
 	GefMoteur::GefSystemSfml::HorlogeSfml horloge;
 	GefMoteur::GefRender::Scene_directeur scdir(horloge);
