3be2b19511713f7bfc27efa7a6491c0c87325e8f
freemaul <freemaul@hotmail.fr>
2010-09-12 19:37:28 +0200

    Creation classe Moteur

--- include/GefMoteur/GefMoteur.hpp
+++ include/GefMoteur/GefMoteur.hpp
@@ -7,9 +7,21 @@ license à ajouter
 #ifndef GEFMOTEUR_HPP
 #define GEFMOTEUR_HPP
 
+#include <GefMoteur/GefRender.hpp>
+
 namespace GefMoteur
 {
 	extern const char Version[];
+
+	class Moteur
+	{
+	protected:
+		GefRender::Scene_directeur& scdir;
+	public:
+		Moteur(GefRender::Scene_directeur&);
+		virtual void Maj();
+		virtual void Dessine();
+	};
 };
 #endif
 
--- src/GefMoteur.cpp
+++ src/GefMoteur.cpp
@@ -1,4 +1,21 @@
 #include <GefMoteur/GefMoteur.hpp>
+using namespace GefMoteur;
+using namespace GefMoteur::GefRender;
 
 const char GefMoteur::Version[]="0.B";
+
+Moteur::Moteur(Scene_directeur& scdir) : scdir(scdir)
+{
+
+}
+
+void Moteur::Maj()
+{
+	scdir.Maj();
+}
+
+void Moteur::Dessine()
+{
+	scdir.Dessine();
+}
 
