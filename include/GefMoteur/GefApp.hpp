28e95327cddcc384ce49f69b4bc27490c9625c26
freemaul <freemaul@hotmail.fr>
2010-09-12 19:54:41 +0200

    Creation de la classe App

---
+++ include/GefMoteur/GefApp.hpp
@@ -0,0 +1,21 @@
+#ifndef GEFMOTEUR_GEFAPP_HPP
+#define GEFMOTEUR_GEFAPP_HPP
+
+#include <GefMoteur/GefRender.hpp>
+
+namespace GefMoteur
+{
+	namespace GefApp
+	{
+		App
+		{
+		protected:
+			GefRender::Scene_directeur &scdir;
+		public:
+			App(GefRender::Scene_directeur&);
+		};
+	};
+};
+
+#endif // GEFMOTEUR__GEFAPP_HPP
+
---
+++ src/GefApp.cpp
@@ -0,0 +1,10 @@
+#include <GefMoteur/GefApp.hpp>
+using namespace GefMoteur;
+using namespace GefMoteur::GefApp;
+using namespace GefMoteur::GefRender;
+
+App::App(Scene_directeur& scdir) : scdir(scdir)
+{
+
+}
+
