b50ea311e8262568b1bd28bb4940e4316bcd88a9
freemaul <freemaul@hotmail.fr>
2010-09-18 11:20:30 +0200

    Creation de la classe MoteurH

---
+++ include/GefMoteur/GefMoteurH.hpp
@@ -0,0 +1,26 @@
+/*
+license à ajouter
+
+
+*/
+
+#ifndef GEFMOTEURH_HPP
+#define GEFMOTEURH_HPP
+
+#include <GefMoteur/GefMoteur.hpp>
+
+namespace GefMoteur
+{
+	class MoteurH : public GefMoteur::Moteur
+	{
+
+	};
+
+	namespace MoteurHClasses
+	{
+
+
+	};
+};
+#endif
+
---
+++ src/GefMoteurH.cpp
@@ -0,0 +1,4 @@
+#include <GefMoteur/GefMoteurH.hpp>
+using namespace GefMoteur;
+using namespace GefMoteur::MoteurHClasses;
+
