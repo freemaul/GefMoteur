96e1af1f91d1f34ca4a23a5840ddc9daf563add3
freemaul <freemaul@hotmail.fr>
2010-10-14 05:21:18 -0700

    Commentaires dans GefApp.h

--- include/GefMoteur/GefApp.hpp
+++ include/GefMoteur/GefApp.hpp
@@ -9,6 +9,12 @@ namespace GefMoteur
 {
 	namespace GefApp
 	{
+		/************************************************************
+		* Classe App
+		* 
+		* Gère l'application, permet de faire une interface entre le moteur
+		*du jeu et le moteur graphique
+		************************************************************/
 		class App
 		{
 		protected:
@@ -16,8 +22,11 @@ namespace GefMoteur
 			GefRender::Ressources_directeur &ressdir;
 			GefMoteur::Moteur &moteur;
 		public:
+			// Constructeur
 			App(GefRender::Scene_directeur&,GefRender::Ressources_directeur&,GefMoteur::Moteur&);
+			// Configuration de l'application
 			virtual void Configure(int,int,int,std::string)=0;
+			// Lancement de l'application
 			virtual void Boucle()=0;
 		};
 	};
