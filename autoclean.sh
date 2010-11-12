6dda19b9f47a4a0ac6d71a11ea455bf7cf14f972
freemaul <freemaul@hotmail.fr>
2010-11-12 13:43:31 +0100

    Ajout de autoclean

---
+++ autoclean.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+rm -f	COPYING
+rm -f	INSTALL
+rm -f	Makefile
+rm -f	Makefile.in
+rm -f	aclocal.m4
+rm 	autom4te.cache/ -dir -f
+rm -f	config.guess
+rm -f	config.h
+rm -f	config.h.in
+rm -f	config.log
+rm -f	config.status
+rm -f	config.sub
+rm -f	configure
+rm -f	depcomp
+rm -f	install-sh
+rm -f	libtool
+rm -f	ltmain.sh
+rm -f	missing
+rm 	src/.deps/ -dir -f
+rm -f	src/Makefile
+rm -f	src/Makefile.in
+rm -f	stamp-h1
+rm 	test/testsfml/.deps/ -dir -f
+rm -f	test/testsfml/Makefile
+rm -f	test/testsfml/Makefile.in
+
